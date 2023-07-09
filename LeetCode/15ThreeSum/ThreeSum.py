05cb3082f3ed438b6a26e6e86ac3e71c46506c12
Varun Seshu <seshuvarun@gmail.com>
2023-07-09 15:51:41 +0530

    Completed ThreeSum

---
+++ LeetCode/15ThreeSum/ThreeSum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Reduce to 2Sum by sorting -> Time:O(n^2), Space:O(nC3)
+        ans = []
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                threesum = nums[i] + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return ans
