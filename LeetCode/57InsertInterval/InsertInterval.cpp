63d1d883a3e684cb4cd01484caa8bf7de99780e0
Varun Seshu <seshuvarun@gmail.com>
2023-07-18 14:25:46 +0530

    Completed InsertInterval

---
+++ LeetCode/57InsertInterval/InsertInterval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // Single pass -> Time:O(n), Space:O(n)
+        vector<vector<int>> ans;
+        for (int i = 0; i < intervals.size(); i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                ans.push_back(newInterval);
+                while (i < intervals.size()) {
+                    ans.push_back(intervals[i]);
+                    i++;
+                }
+                return ans;
+            }
+            else if (newInterval[0] > intervals[i][1]) {
+                ans.push_back(intervals[i]);
+            }
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
