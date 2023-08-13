9140e966449b589b08a381124f9b03486cadfbbe
Varun Seshu <seshuvarun@gmail.com>
2023-08-13 22:16:30 +0530

    Completed ConvertSortedArrayToBinarySearchTree

---
+++ LeetCode/108ConvertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        # Divide and Conquer -> Time:O(n), Space:O(logn)
+        def helper(l, r):
+            if l > r:
+                return None
+            m = (l + r) // 2
+            root = ListNode(nums[m])
+            root.left = helper(l, m-1)
+            root.right = helper(m+1, r)
+            return root
+        return helper(0, len(nums)-1)
