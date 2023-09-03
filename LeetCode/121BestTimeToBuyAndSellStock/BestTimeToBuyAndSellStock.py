6f01039090d5238ce21f4c283098330613314e6a
Varun Seshu <seshuvarun@gmail.com>
2023-09-03 18:15:13 -0400

    Modified BestTimeToBuyAndSellStock

--- LeetCode/121BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.py
+++ LeetCode/121BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.py
@@ -10,14 +10,14 @@ class Solution:
                 max_profit = max(max_profit, price-min_price)
         return max_profit
 
-        # Two pointers -> Time: O(n), Space: O(1)
-        l, r = 0, 1
-        max_profit = 0
-        while r < len(prices):
-            if prices[l] > prices[r]:
-                l = r
-            else:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
-            r += 1
-        return max_profit
+        # # Two pointers -> Time: O(n), Space: O(1)
+        # l, r = 0, 1
+        # max_profit = 0
+        # while r < len(prices):
+        #     if prices[l] > prices[r]:
+        #         l = r
+        #     else:
+        #         profit = prices[r] - prices[l]
+        #         max_profit = max(max_profit, profit)
+        #     r += 1
+        # return max_profit
