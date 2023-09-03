8b0e9165055ea2a8a0f90c5afdf597827fa3d478
Varun Seshu <seshuvarun@gmail.com>
2023-09-03 18:10:08 -0400

    Modified RemoveDuplicatesFromSortedArray

--- LeetCode/26RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.py
+++ LeetCode/26RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.py
@@ -1,6 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # Partition method -> Time: O(n), Space: O(1)
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+        # Brute Force: Time: O(n), Space: O(n)
         k = 0
         s = set()
         for i in range(len(nums)):
