924d2407e824743d72709738a0d8a7ceaf24c0a8
Varun Seshu <seshuvarun@gmail.com>
2023-10-14 15:01:00 -0400

    Completed SubTreeOfAnotherTree

---
+++ NeetCodeIO/572SubTreeOfAnotherTree/SubTreeOfAnotherTree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sameTree(self, root, subRoot):
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
+        return False
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # DFS -> Time:O(n*m), Space:O(n*m)
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.sameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
